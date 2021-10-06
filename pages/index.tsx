6045860cec9980cb737be137c1510fdb4f5e03e4
Gafur Iusupaliev <igafurp@gmail.com>
2021-10-05 23:04:54 -0400

    Displaying user's email once logged in

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,12 @@ import { getSession, useSession } from "next-auth/react";
 import router from "next/router";
 
 const Home: NextPage = () => {
-  const { status } = useSession();
-  console.log(status);
+  const { data, status } = useSession();
+
   if (status === "unauthenticated") {
     router.push("/api/auth/signin");
   } else if (status === "authenticated") {
-    return <h1>hi user</h1>;
+    return <h1>hi user with the email &quot;{data?.user?.email}&quot;</h1>;
   }
 
   return <h1>loading</h1>;
