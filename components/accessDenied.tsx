3dce2a911e368e6ff1d056a90743ee1eb0756acd
Gafur Iusupaliev <igafurp@gmail.com>
2021-10-20 19:39:29 -0400

    Changed button color and spaced items out

--- components/accessDenied.tsx
+++ components/accessDenied.tsx
@@ -1,12 +1,17 @@
 import { Button } from "@chakra-ui/button";
 import { Center, Heading } from "@chakra-ui/layout";
+import { VStack } from "@chakra-ui/react";
 import { signIn } from "next-auth/react";
 
 export default function AccessDenied() {
   return (
     <Center minH="100vh" minW="100vw" flexDirection="column">
-      <Heading>you are not signed in</Heading>{" "}
-      <Button onClick={() => signIn()}>Sign in</Button>
+      <VStack spacing={10}>
+        <Heading>You are not signed in.</Heading>{" "}
+        <Button size="lg" onClick={() => signIn()} colorScheme="blue">
+          Sign in
+        </Button>
+      </VStack>
     </Center>
   );
 }
