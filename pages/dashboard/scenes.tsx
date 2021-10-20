6819971a3be384dc9bfe4a250996b2e2c4b2106e
Gafur Iusupaliev <igafurp@gmail.com>
2021-10-20 18:41:25 -0400

    Improved how the UI looks

--- pages/dashboard/scenes.tsx
+++ pages/dashboard/scenes.tsx
@@ -1,7 +1,15 @@
 import { Button } from "@chakra-ui/button";
-import { Box, Code, Flex, Heading, VStack } from "@chakra-ui/layout";
+import {
+  Box,
+  Code,
+  Flex,
+  Heading,
+  HStack,
+  Text,
+  VStack,
+} from "@chakra-ui/layout";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import AccessDenied from "../../components/accessDenied";
 import { v4 as uuidv4 } from "uuid";
 import {
@@ -18,26 +26,29 @@ import {
 } from "@chakra-ui/react";
 import { useDisclosure } from "@chakra-ui/hooks";
 import { Input } from "@chakra-ui/input";
+import { Textarea } from "@chakra-ui/react";
 
 export default function Scenes() {
   interface scene {
     id: string;
     sceneName: string;
-    sceneDesc: string;
+    sceneDesc: string | undefined;
     includeTrashcan: boolean;
     hydrogenPeroxide: boolean;
     alcohol: boolean;
   }
 
-  const { data } = useSession();
+  const { data: session, status } = useSession();
 
-  const [sceneName, setSceneName] = useState("");
-  const [sceneDesc, setSceneDesc] = useState("");
-  const [includeTrashcan, setIncludeTrashcan] = useState(false);
-  const [hydrogenPeroxide, setHydrogenPeroxide] = useState(false);
-  const [alcohol, setAlcohol] = useState(false);
-  const [currentSceneOpen, setCurrentSceneOpen] = useState<scene>();
   const [scenes, setScenes] = useState<scene[]>([]);
+
+  const sceneName = useRef<HTMLInputElement>(null);
+  const sceneDesc = useRef<HTMLTextAreaElement>(null);
+  const incluldeTrashcan = useRef<HTMLInputElement>(null);
+  const hydrogenPeroxide = useRef<HTMLInputElement>(null);
+  const alcohol = useRef<HTMLInputElement>(null);
+
+  const [currentSceneOpen, setCurrentSceneOpen] = useState<scene>();
 
   const {
     isOpen: isOpenSceneInfo,
@@ -51,92 +62,91 @@ export default function Scenes() {
     onClose: onCloseSceneJson,
   } = useDisclosure();
 
-  if (!data?.token) {
+  if (status === "unauthenticated") {
     return <AccessDenied />;
   }
   return (
-    <Box ml="15rem">
+    <Box ml="20vw" maxWidth="80vw">
       <Heading textAlign="center">Scenes</Heading>
-      <Button onClick={onOpenSceneInfo}>Add Scene</Button>
+      <Button onClick={onOpenSceneInfo} colorScheme="green">
+        Add Scene
+      </Button>
 
-      <Modal isOpen={isOpenSceneInfo} onClose={onCloseSceneInfo}>
+      <Modal isOpen={isOpenSceneInfo} onClose={onCloseSceneInfo} size="lg">
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader>Create a scene</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <FormControl>
-              <FormLabel>Scene name</FormLabel>
-              <Input
-                variant="flushed"
-                type="text"
-                onChange={(e) => setSceneName(e.target.value)}
-                value={sceneName}
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel>Scene description</FormLabel>
-              <Input
-                variant="flushed"
-                type="text"
-                onChange={(e) => setSceneDesc(e.target.value)}
-                value={sceneDesc}
-              />
-            </FormControl>
+            <VStack spacing={3} mt="1rem" mb="1rem">
+              <FormControl isRequired>
+                <FormLabel>Scene Name</FormLabel>
+                <Input type="text" ref={sceneName} placeholder="H-bond" />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Scene Description</FormLabel>
+                <Textarea
+                  type="text"
+                  ref={sceneDesc}
+                  placeholder="This scene will be used to demonstrate hydrogen bonding..."
+                />
+              </FormControl>
+            </VStack>
 
-            <Checkbox
-              size="lg"
-              isChecked={includeTrashcan}
-              onChange={() => setIncludeTrashcan((oldVal) => !oldVal)}
-            >
-              Include Trashcan
-            </Checkbox>
-            <Checkbox
-              size="lg"
-              isChecked={hydrogenPeroxide}
-              onChange={() => setHydrogenPeroxide((oldVal) => !oldVal)}
-            >
-              Hydrogen Peroxide
-            </Checkbox>
-            <Checkbox
-              size="lg"
-              isChecked={alcohol}
-              onChange={() => setAlcohol((oldVal) => !oldVal)}
-            >
-              Alcohol
-            </Checkbox>
+            <Flex wrap="wrap">
+              <Checkbox mr="1rem" mt="1rem" size="lg" ref={incluldeTrashcan}>
+                Include Trashcan
+              </Checkbox>
+              <Checkbox mr="1rem" mt="1rem" size="lg" ref={hydrogenPeroxide}>
+                Hydrogen Peroxide
+              </Checkbox>
+              <Checkbox mr="1rem" mt="1rem" size="lg" ref={alcohol}>
+                Alcohol
+              </Checkbox>
+            </Flex>
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onCloseSceneInfo}>
-              Close
-            </Button>
-            <Button
-              variant="ghost"
-              onClick={() => {
-                onCloseSceneInfo();
-                setScenes((oldArr) => {
-                  return [
-                    {
-                      id: `${sceneName} ${sceneDesc}`,
-                      sceneDesc,
-                      sceneName,
-                      includeTrashcan,
-                      hydrogenPeroxide,
-                      alcohol,
-                    },
-                    ...oldArr,
-                  ];
-                });
-                setSceneName("");
-                setSceneDesc("");
-                setIncludeTrashcan(false);
-                setHydrogenPeroxide(false);
-                setAlcohol(false);
-              }}
-            >
-              Add Scene
-            </Button>
+            <HStack spacing={5}>
+              <Button
+                colorScheme="red"
+                variant="ghost"
+                mr={3}
+                onClick={onCloseSceneInfo}
+              >
+                Close
+              </Button>
+              <Button
+                colorScheme="green"
+                onClick={() => {
+                  onCloseSceneInfo();
+                  setScenes((oldArr) => {
+                    if (
+                      !sceneName.current?.value ||
+                      !incluldeTrashcan.current ||
+                      !hydrogenPeroxide.current ||
+                      !alcohol.current
+                    ) {
+                      return [...oldArr];
+                    } else {
+                      return [
+                        {
+                          id: `${sceneName.current?.value} ${sceneDesc.current?.value}`,
+                          sceneName: sceneName.current?.value,
+                          sceneDesc: sceneDesc.current?.value,
+                          includeTrashcan: incluldeTrashcan.current?.checked,
+                          hydrogenPeroxide: hydrogenPeroxide.current?.checked,
+                          alcohol: alcohol.current?.checked,
+                        },
+                        ...oldArr,
+                      ];
+                    }
+                  });
+                }}
+              >
+                Add Scene
+              </Button>
+            </HStack>
           </ModalFooter>
         </ModalContent>
       </Modal>
@@ -150,21 +160,30 @@ export default function Scenes() {
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onCloseSceneJson}>
-              Close
-            </Button>
-            <Button
-              variant="ghost"
-              onClick={() =>
-                navigator.clipboard.writeText(JSON.stringify(currentSceneOpen))
-              }
-            >
-              Copy JSON
-            </Button>
+            <HStack spacing={5}>
+              <Button
+                variant="ghost"
+                colorScheme="red"
+                mr={3}
+                onClick={onCloseSceneJson}
+              >
+                Close
+              </Button>
+              <Button
+                colorScheme="blue"
+                onClick={() =>
+                  navigator.clipboard.writeText(
+                    JSON.stringify(currentSceneOpen)
+                  )
+                }
+              >
+                Copy JSON
+              </Button>
+            </HStack>
           </ModalFooter>
         </ModalContent>
       </Modal>
-      <Flex flexDirection="column">
+      <Flex wrap="wrap" flexDirection="row" mt="1rem" width="80vw">
         {scenes.map(
           ({
             sceneName,
@@ -176,33 +195,80 @@ export default function Scenes() {
             <VStack
               key={`${uuidv4()}`}
               alignItems="flex-start"
+              flexWrap="wrap"
               id={`${sceneName} ${sceneDesc}`}
+              mt="1rem"
+              mb="1rem"
+              boxShadow="xs"
+              padding="2rem"
+              borderRadius="1rem"
+              minW="78vw"
             >
-              <Heading>Name: {sceneName}</Heading>
-              <Heading>Description: {sceneDesc}</Heading>
-              <Heading>Options (read only)</Heading>
-              <Checkbox isReadOnly isChecked={includeTrashcan}>
-                Trascan Included
-              </Checkbox>
-              <Checkbox isReadOnly isChecked={hydrogenPeroxide}>
-                Start With Hydrogen Peroxide
-              </Checkbox>
-              <Checkbox isReadOnly isChecked={alcohol}>
-                Disable Alcohol
-              </Checkbox>
-              <Button
-                onClick={(e) => {
-                  onOpenSceneJson();
-                  const target = e.currentTarget;
-                  const parentId = target.parentElement?.id;
+              <Heading>{sceneName}</Heading>
+              <Text mt="1rem" mb="1rem">
+                {sceneDesc}
+              </Text>
 
-                  setCurrentSceneOpen(
-                    scenes.find((scene) => scene.id === parentId)
-                  );
-                }}
-              >
-                View Json
-              </Button>
+              <Flex wrap="wrap" justifyContent="flex-start">
+                <Checkbox
+                  isReadOnly
+                  isChecked={includeTrashcan}
+                  mr="1rem"
+                  mt="1rem"
+                  mb="1rem"
+                >
+                  Trascan Included
+                </Checkbox>
+                <Checkbox
+                  isReadOnly
+                  isChecked={hydrogenPeroxide}
+                  mr="1rem"
+                  mt="1rem"
+                  mb="1rem"
+                >
+                  Start With Hydrogen Peroxide
+                </Checkbox>
+                <Checkbox
+                  isReadOnly
+                  isChecked={alcohol}
+                  mr="1rem"
+                  mt="1rem"
+                  mb="1rem"
+                >
+                  Disable Alcohol
+                </Checkbox>
+              </Flex>
+              <HStack spacing={5}>
+                <Button
+                  onClick={(e) => {
+                    const target = e.currentTarget;
+                    const parentId = target.parentElement;
+                    const grandParentId = parentId?.parentElement?.id;
+
+                    setCurrentSceneOpen(() =>
+                      scenes.find((scene) => scene.id === grandParentId)
+                    );
+                    onOpenSceneJson();
+                  }}
+                  colorScheme="blue"
+                >
+                  View Json
+                </Button>
+                <Button
+                  onClick={(e) => {
+                    const target = e.currentTarget;
+                    const parentId = target.parentElement;
+                    const grandParentId = parentId?.parentElement?.id;
+
+                    setScenes((oldScenes) =>
+                      oldScenes.filter((scene) => scene.id != grandParentId)
+                    );
+                  }}
+                  colorScheme="red"
+                >
+                  Delete Scene
+                </Button>
+              </HStack>
             </VStack>
           )
         )}
