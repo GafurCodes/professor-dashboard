059283068fda16adc5232eca549a2207c34c7051
Gafur Iusupaliev <igafurp@gmail.com>
2021-10-21 14:55:06 -0400

    Changed menu's positino from absolute to fixed and converted units to rem

--- components/navMenu.tsx
+++ components/navMenu.tsx
@@ -3,22 +3,21 @@ import { Button } from "@chakra-ui/button";
 import { HStack, Text, VStack } from "@chakra-ui/layout";
 import { useSession } from "next-auth/react";
 import router from "next/router";
-
+import { GiJigsawBox } from "react-icons/gi";
 export default function NavMenu() {
   const { data } = useSession();
-  console.log(data);
   const firstName = data?.token.firstName;
   const lastName = data?.token.lastName;
 
   return (
     <VStack
       mt="1rem"
-      position="absolute"
       minH="100vh"
       h="100%"
       left="0"
       top="0"
-      minW="17vw"
+      w="15rem"
+      position="fixed"
       boxShadow="xl"
       spacing={10}
     >
@@ -28,7 +27,14 @@ export default function NavMenu() {
           {firstName} {lastName}
         </Text>
       </HStack>
-      <Button onClick={() => router.push("/dashboard/scenes")}>Scenes</Button>
+      <VStack spacing={5}>
+        <Button
+          leftIcon={<GiJigsawBox />}
+          onClick={() => router.push("/dashboard/scenes")}
+        >
+          Scenes
+        </Button>
+      </VStack>
     </VStack>
   );
 }
