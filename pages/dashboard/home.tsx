4e22b4fda833233eb772efa0cf58f33519bedfe4
Gafur Iusupaliev <igafurp@gmail.com>
2021-10-21 14:59:30 -0400

    Removed auth check that I moved to the layout component

--- pages/dashboard/home.tsx
+++ pages/dashboard/home.tsx
@@ -1,62 +1,9 @@
 import { Box, Heading } from "@chakra-ui/layout";
-import { useSession } from "next-auth/react";
-import AccessDenied from "../../components/accessDenied";
 
 export default function Dashboard() {
-  const { data: session, status } = useSession();
-
-  if (status === "unauthenticated") {
-    return <AccessDenied />;
-  }
   return (
-    <Box ml="15rem">
+    <Box>
       <Heading textAlign="center">Dashboard</Heading>
     </Box>
   );
 }
-
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   const session = await getSession(context);
-//   if (!session) {
-//     return {
-//       props: {
-//         userData: {
-//           error: "iserror",
-//         },
-//       },
-//     };
-//   }
-
-//   const { user } = session;
-
-//   const userFromDb = await prisma.user.findFirst({
-//     where: {
-//       email: user?.email!,
-//     },
-//   });
-
-//   const stringifiedData = safeJsonStringify(userFromDb!);
-
-//   const userData = JSON.parse(stringifiedData);
-//   return {
-//     props: {
-//       userData: {
-//         email: userData.email,
-//         firstName: userData.firstName,
-//         lastName: userData.lastName,
-//       },
-//     },
-//   };
-// };
-
-// if (!userData.error) {
-//   return <NavMenu userData={userData} />;
-// } else if (userData.error) {
-//   return (
-//     <>
-//       <h1>you are not authenticated</h1>
-//       <button onClick={() => signIn()}> sign in</button>
-//     </>
-//   );
-// }
-// return <h1>loading</h1>;
