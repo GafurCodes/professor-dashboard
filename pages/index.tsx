b90e8cad803863f2adee08952172bdd066ca88a6
Gafur Iusupaliev <igafurp@gmail.com>
2021-10-21 16:02:55 -0400

    Home page now routes to the dashboard if the user is authenticated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 // import Head from "next/head"; will add later for SEO
 import { useSession } from "next-auth/react";
+
 import router from "next/router";
 import AccessDenied from "../components/accessDenied";
 import LoadingSpinner from "../components/loadingSpinner";
@@ -10,6 +11,9 @@ const Home: NextPage = () => {
 
   if (status === "unauthenticated") {
     return <AccessDenied />;
+  }
+  if (status === "authenticated") {
+    router.push("/dashboard/home");
   }
 
   return <LoadingSpinner />;
